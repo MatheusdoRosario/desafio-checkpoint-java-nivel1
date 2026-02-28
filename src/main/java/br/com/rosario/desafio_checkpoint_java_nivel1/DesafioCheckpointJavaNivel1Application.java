f5ee069acecb15ac989860ab402034aed767c612
MatheusDoRosario <matheusxdargws@gmail.com>
2026-02-28 12:39:35 -0300

    Initial commit

---
+++ src/main/java/br/com/rosario/desafio_checkpoint_java_nivel1/DesafioCheckpointJavaNivel1Application.java
@@ -0,0 +1,13 @@
+package br.com.rosario.desafio_checkpoint_java_nivel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCheckpointJavaNivel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCheckpointJavaNivel1Application.class, args);
+	}
+
+}
