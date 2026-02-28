295c0644e8c692201ec7e102358fbb5726886c21
MatheusDoRosario <matheusxdargws@gmail.com>
2026-02-28 14:42:50 -0300

    Adiciona UsuarioRepository

---
+++ src/main/java/br/com/rosario/desafio_checkpoint_java_nivel1/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.rosario.desafio_checkpoint_java_nivel1.repository;
+
+import br.com.rosario.desafio_checkpoint_java_nivel1.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Long, Usuario> {
+}
