24d49b55f9327b718b4f5a01c5a7058c74e0d697
MatheusDoRosario <matheusxdargws@gmail.com>
2026-02-28 14:43:50 -0300

    Adiciona o ReservaRepository

---
+++ src/main/java/br/com/rosario/desafio_checkpoint_java_nivel1/repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package br.com.rosario.desafio_checkpoint_java_nivel1.repository;
+
+import br.com.rosario.desafio_checkpoint_java_nivel1.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ReservaRepository extends JpaRepository<UUID, Reserva> {
+}
