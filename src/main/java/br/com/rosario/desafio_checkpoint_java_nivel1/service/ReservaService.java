6830bdda0ae2ee8f9862428a8072bb1331035f91
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-02 11:48:44 -0300

    Otimizacao no ReservaService

--- src/main/java/br/com/rosario/desafio_checkpoint_java_nivel1/service/ReservaService.java
+++ src/main/java/br/com/rosario/desafio_checkpoint_java_nivel1/service/ReservaService.java
@@ -11,7 +11,6 @@ import br.com.rosario.desafio_checkpoint_java_nivel1.repository.SalaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
